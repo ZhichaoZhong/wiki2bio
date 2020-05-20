8c85604b4cb18fe6b52310a65c2db472b8d8220d
ZC Zhong <>
2020-05-20 16:26:03 +0200

    fix

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
     author_email='zzhong@wehkamp.nl',
     description='Package for wiki2bio, check the master branch for the orignal code.',
     install_requires = ['tensorflow==1.13.1'],
-    packages=find_packages('./wiki2bio', exclude=['original_data', 'doc']),
+    packages=find_packages('wiki2bio', exclude=['original_data', 'doc']),
     python_requires='>=3.7',
 )
