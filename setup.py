91fe6f1deb922364f5e08ad5c5639791626ccaaa
ZC Zhong <>
2020-05-20 16:38:20 +0200

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-from setuptools import find_packages
+
 setup(
     name='wiki2bio',
     version='0.0.1',
@@ -11,6 +11,6 @@ setup(
     author_email='zzhong@wehkamp.nl',
     description='Package for wiki2bio, check the master branch for the orignal code.',
     install_requires = ['tensorflow==1.13.1'],
-    packages=find_packages('wiki2bio', exclude=['original_data', 'doc']),
+    packages=["wiki2bio"],
     python_requires='>=3.7',
 )
